79ab4e1f8172f85980c8ff1d152d4df66d6c3d0e
TuanKhanh <khanh.nguyen1909@hcmut.edu.vn>
2025-09-15 11:11:17 +0700

    Ex2

--- LAB2/lab2/Core/Inc/main.h
+++ LAB2/lab2/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define DOT_Pin GPIO_PIN_4
+#define DOT_GPIO_Port GPIOA
 #define RED_LED_Pin GPIO_PIN_5
 #define RED_LED_GPIO_Port GPIOA
 #define EN0_Pin GPIO_PIN_6
@@ -69,6 +71,10 @@ void Error_Handler(void);
 #define Led7_b_GPIO_Port GPIOB
 #define Led7_c_Pin GPIO_PIN_2
 #define Led7_c_GPIO_Port GPIOB
+#define EN2_Pin GPIO_PIN_8
+#define EN2_GPIO_Port GPIOA
+#define EN3_Pin GPIO_PIN_9
+#define EN3_GPIO_Port GPIOA
 #define Led7_d_Pin GPIO_PIN_3
 #define Led7_d_GPIO_Port GPIOB
 #define Led7_e_Pin GPIO_PIN_4
